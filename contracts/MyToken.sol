0c44cfb5ad6357a4a63c930d12cadd06319d68b4
AnanyaSharma29 <96763584+AnanyaSharma29@users.noreply.github.com>
2024-07-17 11:12:38 +0530

    MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    constructor() ERC20("Avni", "AC") {
+    constructor() ERC20("Ananya", "AS") {
         // Initialize predefined items in the constructor
         _addPredefinedItem(1, "Item 1", 100);
         _addPredefinedItem(2, "Item 2", 200);
